690f9da7ff16bc4b0f07d486feafa395f4ce1419
fahadNtoron <fahad.hossenxy@gmail.com>
2024-11-06 15:41:57 +0600

    Enviroment setting done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black">ok</div>
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  )
+}
+
+export default App
